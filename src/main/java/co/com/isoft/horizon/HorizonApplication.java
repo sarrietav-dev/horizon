f915cea8841c27e695ac2b77cbf6d31a0e2b2dfa
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-04-27 19:11:42 -0500

    add application properties

--- src/main/java/co/com/isoft/horizon/HorizonApplication.java
+++ src/main/java/co/com/isoft/horizon/HorizonApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HorizonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HorizonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HorizonApplication.class, args);
+    }
 
 }
