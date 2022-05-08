a3cdbd0ae8f985650ff4fa51079f828381173ce0
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-07 19:28:40 -0500

    Create user controller

--- src/main/java/co/com/isoft/horizon/controllers/UserController.java
+++ src/main/java/co/com/isoft/horizon/controllers/UserController.java
@@ -1,21 +1,38 @@
 package co.com.isoft.horizon.controllers;
 
+import co.com.isoft.horizon.DTO.UserDTO;
+import co.com.isoft.horizon.models.Person;
+import co.com.isoft.horizon.models.Resident;
+import co.com.isoft.horizon.models.Role;
 import co.com.isoft.horizon.models.User;
-import co.com.isoft.horizon.repositories.UserRepo;
+import co.com.isoft.horizon.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController(value = "/api/users")
 public class UserController {
-    final UserRepo userRepo;
+    final UserService userService;
 
-    public UserController(UserRepo userRepo) {
-        this.userRepo = userRepo;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/")
-    public User createUser(@RequestBody User user) {
-        return userRepo.save(user);
+    public User createUser(@RequestBody UserDTO userDTO) {
+        User user = User.from(userDTO);
+        switch (user.getRole()) {
+            case ADMIN:
+            case PROPRIETARY:
+                break;
+            case RESIDENT:
+                Person person = Resident.from(userDTO.getUserData());
+                user.setUserData(person);
+                person.setAuthData(user);
+                break;
+        }
+
+        return userService.createUser(user);
     }
 }
