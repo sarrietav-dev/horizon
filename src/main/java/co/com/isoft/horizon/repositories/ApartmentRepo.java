4b8f4b9fd43479bec1661e0f642b0feeda9e6014
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-10 09:11:57 -0500

    Rename the file

--- src/main/java/co/com/isoft/horizon/repositories/ApartmentRepo.java
+++ src/main/java/co/com/isoft/horizon/repositories/ApartmentRepo.java
@@ -3,5 +3,5 @@ package co.com.isoft.horizon.repositories;
 import co.com.isoft.horizon.models.Apartment;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ApartamentoRepo extends CrudRepository<Apartment, Long> {
+public interface ApartmentRepo extends CrudRepository<Apartment, Long> {
 }
