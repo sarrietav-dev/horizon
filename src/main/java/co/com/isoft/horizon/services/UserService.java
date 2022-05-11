94fdbb2e9e5cfbcf20570fff5d4f57b2464a7ae8
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-10 21:03:02 -0500

    Encrypt password when creating a user

--- src/main/java/co/com/isoft/horizon/services/UserService.java
+++ src/main/java/co/com/isoft/horizon/services/UserService.java
@@ -2,24 +2,26 @@ package co.com.isoft.horizon.services;
 
 import co.com.isoft.horizon.models.User;
 import co.com.isoft.horizon.repositories.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
-@Service
+@Service @Slf4j @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
     final UserRepo userRepo;
-
-    public UserService(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
+    final PasswordEncoder passwordEncoder;
 
     public User createUser(User user) {
+        log.info("Saving new user {} to the database", user.getEmail());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
