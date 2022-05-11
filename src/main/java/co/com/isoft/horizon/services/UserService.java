3d7df547691d051d296f099d96ec93648ac4ea7c
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-10 19:51:51 -0500

    Implement UserDetailsService interface

--- src/main/java/co/com/isoft/horizon/services/UserService.java
+++ src/main/java/co/com/isoft/horizon/services/UserService.java
@@ -2,11 +2,17 @@ package co.com.isoft.horizon.services;
 
 import co.com.isoft.horizon.models.User;
 import co.com.isoft.horizon.repositories.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     final UserRepo userRepo;
 
     public UserService(UserRepo userRepo) {
@@ -15,5 +21,19 @@ public class UserService {
 
     public User createUser(User user) {
         return userRepo.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found in the database");
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority(user.getRole().toString()));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
     }
 }
