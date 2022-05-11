fe6ae2d27560aa72ce792246d9777842fbfc4dd6
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-10 19:51:27 -0500

    Define findByEmail method

--- src/main/java/co/com/isoft/horizon/repositories/UserRepo.java
+++ src/main/java/co/com/isoft/horizon/repositories/UserRepo.java
@@ -3,5 +3,8 @@ package co.com.isoft.horizon.repositories;
 import co.com.isoft.horizon.models.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepo extends CrudRepository<User, Long> {
+    User findByEmail(String email);
 }
