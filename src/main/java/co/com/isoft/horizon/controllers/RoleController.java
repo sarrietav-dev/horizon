cf6716c14ac55d779c6ff78fbb4f21f7e1dd1cf5
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-10 23:19:31 -0500

    use explicit Dependency Injection

--- src/main/java/co/com/isoft/horizon/controllers/RoleController.java
+++ src/main/java/co/com/isoft/horizon/controllers/RoleController.java
@@ -3,23 +3,27 @@ package co.com.isoft.horizon.controllers;
 import co.com.isoft.horizon.models.Role;
 import co.com.isoft.horizon.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriBuilder;
 
 import java.net.URI;
 
 @RestController
 @RequestMapping("/api/role")
-@RequiredArgsConstructor
 public class RoleController {
+    final
     UserService userService;
 
-    @PostMapping("/")
+    public RoleController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
     ResponseEntity<Role> createRole(@RequestBody Role role) {
         Role newRole = userService.saveRole(role);
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/").toUriString());
