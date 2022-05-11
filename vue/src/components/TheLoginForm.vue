e815e60730864f80b1c25314dd4617b2852625da
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-11 18:47:12 -0500

    Create TheLoginForm.vue component

---
+++ vue/src/components/TheLoginForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h2 class="text-center my-3">Iniciar Sesion</h2>
+      <form class="container" method="POST">
+        <div class="row my-2">
+          <div class="col">
+            <input
+              aria-label="Email"
+              class="form-control"
+              placeholder="Email"
+              type="email"
+            />
+          </div>
+        </div>
+        <div class="row my-2">
+          <div class="col">
+            <input
+              aria-label="Contraseña"
+              class="form-control"
+              placeholder="Contraseña"
+              type="password"
+            />
+          </div>
+        </div>
+        <div class="d-flex justify-content-center align-items-center">
+          <button class="btn btn-primary" type="submit">Iniciar Sesion</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
