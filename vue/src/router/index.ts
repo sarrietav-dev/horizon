2ae27e5264291a24ec913e7aafc57dc8f179392a
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-12 00:04:01 -0500

    Add global route guard

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import { useAuthStore } from "@/stores/authStore";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,12 @@ const router = createRouter({
       component: () => import("../views/LoginView.vue"),
     },
   ],
+});
+
+router.beforeEach((to) => {
+  const auth = useAuthStore();
+
+  if (!auth.isAuthenticated && to.name !== "login") return { name: "login" };
 });
 
 export default router;
--- vue/src/stores/authStore.ts
+++ vue/src/stores/authStore.ts
@@ -4,17 +4,23 @@ export const ACCESS_TOKEN = "access_token";
 export const REFRESH_TOKEN = "refresh_token";
 
 export const useAuthStore = defineStore("tokenManager", {
+  state: () => ({
+    accessToken: "",
+    refreshToken: "",
+  }),
   actions: {
     setAccessToken(token: string) {
+      this.accessToken = token;
       return localStorage.setItem(ACCESS_TOKEN, token);
     },
     setRefreshToken(token: string) {
+      this.refreshToken = token;
       return localStorage.setItem(REFRESH_TOKEN, token);
     },
   },
   getters: {
     isAuthenticated(): boolean {
-      return this.getAccessToken !== null && this.getRefreshToken !== null;
+      return this.accessToken !== null && this.refreshToken !== null;
     },
     getAccessToken: () => localStorage.getItem(ACCESS_TOKEN),
     getRefreshToken: () => localStorage.getItem(REFRESH_TOKEN),
