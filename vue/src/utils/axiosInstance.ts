8558f8a5bbad402205fb4bceb5f8a3b28930f38a
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-14 10:42:50 -0500

    docs: add intent comment to axiosInstance.ts

--- vue/src/utils/axiosInstance.ts
+++ vue/src/utils/axiosInstance.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+/**
+ * Used to reduce code duplication.
+ * If the base url for the api changes,
+ * there's only one place where it needs to be changed.
+ */
 export const instance = axios.create({
   baseURL: "/api",
 });
