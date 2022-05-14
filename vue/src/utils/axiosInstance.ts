51e377617336de147e6f500e273ef77248731d43
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-14 12:04:13 -0500

    refactor: fetch access token when expired

--- vue/src/utils/axiosInstance.ts
+++ vue/src/utils/axiosInstance.ts
@@ -1,10 +1,52 @@
 import axios from "axios";
+import { useAuthStore } from "@/stores/authStore";
 
 /**
  * Used to reduce code duplication.
  * If the base url for the api changes,
  * there's only one place where it needs to be changed.
  */
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: "/api",
 });
+
+axiosInstance.interceptors.request.use(
+  (req) => {
+    const auth = useAuthStore();
+
+    if (req.headers) {
+      req.headers.Authorization = `Bearer ${auth.accessToken}`;
+    }
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const auth = useAuthStore();
+
+    const originalRequest = error.config;
+
+    if (error.response) {
+      if (error.status === 403 && originalRequest._retry) {
+        originalRequest._retry = true;
+
+        const response = await axiosInstance.get("/auth/token", {
+          headers: {
+            Authorization: auth.refreshToken,
+          },
+        });
+
+        const { headers, status } = response;
+
+        if (status === 200) {
+          const { accessToken } = headers;
+          auth.setAccessToken(accessToken);
+        }
+      }
+    }
+  }
+);
+
+export default axiosInstance;
