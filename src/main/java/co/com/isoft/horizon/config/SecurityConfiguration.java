dbf6f86c3520c88b8c0c7af997e49163a82822e2
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-14 19:34:52 -0500

    Add security to the GET /pqrs endpoint

--- src/main/java/co/com/isoft/horizon/config/SecurityConfiguration.java
+++ src/main/java/co/com/isoft/horizon/config/SecurityConfiguration.java
@@ -39,6 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests().antMatchers("/api/login", "/api/auth/token").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/pqrs").hasAnyAuthority();
         http.authorizeRequests().antMatchers(HttpMethod.PATCH, "/api/user/**/role").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/**").hasAnyAuthority("ROLE_ADMIN");
