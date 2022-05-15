8d9cc07e81c7b26a7f70dbf511e01360d29de3f1
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-15 09:58:29 -0500

    chore: create token service class

---
+++ react/src/services/tokenService.ts
@@ -0,0 +1,31 @@
+class TokenService {
+  private readonly accessTokenKey = "accessToken";
+  private readonly refreshTokenKey = "refreshToken";
+
+  get accessToken() {
+    const accessToken = localStorage.getItem(this.accessTokenKey);
+    if (!accessToken) throw new Error(`${this.accessTokenKey} not set`);
+    return JSON.parse(accessToken);
+  }
+
+  set accessToken(token: string) {
+    localStorage.setItem(this.accessTokenKey, token);
+  }
+
+  get refreshToken() {
+    const refreshToken = localStorage.getItem(this.refreshTokenKey);
+    if (!refreshToken) throw new Error(`${this.refreshTokenKey} not set`);
+    return JSON.parse(refreshToken);
+  }
+
+  set refreshToken(token: string) {
+    localStorage.setItem(this.refreshTokenKey, token);
+  }
+
+  deleteTokens() {
+    this.accessToken = ""
+    this.refreshToken = ""
+  }
+}
+
+export default new TokenService();
