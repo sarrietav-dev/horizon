1fdc93ddfb7a6fc7eebb25caf79c6608947f91a9
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-15 09:28:40 -0500

    fix: center login view

--- react/src/views/LoginView.tsx
+++ react/src/views/LoginView.tsx
@@ -1,5 +1,7 @@
 import TheLoginForm from "@/components/TheLoginForm";
 
-const LoginView = () => <TheLoginForm/>
+const LoginView = () => <main className="d-flex justify-content-center align-items-center vh-100">
+  <TheLoginForm/>
+</main>
 
 export default LoginView;
