1e2365cd5556cf7ea0ae831f9c6a529e50135a90
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-15 10:47:50 -0500

    chore: add proxy server and use url instead of path package

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,15 +1,23 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from "path";
+import {fileURLToPath, URL} from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias:
-      {
-        '@': resolve(__dirname, '/src')
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
       }
-
+    }
   }
 })
