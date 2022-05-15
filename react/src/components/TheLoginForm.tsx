cf87723c1163ee1293c4503a183c785afbb408a9
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-15 10:45:39 -0500

    feat: add functionality to the LoginView

--- react/src/components/TheLoginForm.tsx
+++ react/src/components/TheLoginForm.tsx
@@ -1,19 +1,31 @@
-const TheLoginForm = () =>
-  <div>
+import React, {FormEvent, useState} from "react";
+
+interface TheLoginFormProps {
+  onSubmit: (e: FormEvent, ...args: any) => void
+}
+
+const TheLoginForm: React.FC<TheLoginFormProps> = ({onSubmit}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return <div>
     <div className="card-body">
       <h2 className="text-center my-3">Iniciar Sesion</h2>
-      <form method="POST" className="container">
+      <form method="POST" className="container" onSubmit={(e) => onSubmit(e, email, password)}>
         <div className="row my-2">
-          <div className="col"><input type="email" className="form-control"/></div>
+          <div className="col"><input value={email} onChange={(e) => setEmail(e.target.value)} type="email"
+                                      className="form-control"/></div>
         </div>
         <div className="row my-2">
-          <div className="col"><input type="password" className="form-control"/></div>
+          <div className="col"><input value={password} onChange={e => setPassword(e.target.value)} type="password"
+                                      className="form-control"/></div>
         </div>
         <div className="d-flex justify-content-center align-items-center">
           <button className="btn btn-primary">Iniciar Sesion</button>
         </div>
       </form>
     </div>
-  </div>
+  </div>;
+}
 
 export default TheLoginForm;
--- react/src/views/LoginView.tsx
+++ react/src/views/LoginView.tsx
@@ -1,7 +1,26 @@
 import TheLoginForm from "@/components/TheLoginForm";
+import {authenticateUser} from "@/services/auth";
+import {useNavigate} from "react-router-dom";
+import {FormEvent} from "react";
+import tokenService from "@/services/tokenService";
 
-const LoginView = () => <main className="d-flex justify-content-center align-items-center vh-100">
-  <TheLoginForm/>
-</main>
+const LoginView = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: FormEvent, email: string, password: string) => {
+    e.preventDefault();
+
+    const {accessToken, refreshToken} = await authenticateUser(email, password);
+
+    tokenService.accessToken = accessToken;
+    tokenService.refreshToken = refreshToken;
+
+    navigate("/", {replace: true});
+  }
+
+  return <main className="d-flex justify-content-center align-items-center vh-100">
+    <TheLoginForm onSubmit={handleSubmit}/>
+  </main>;
+}
 
 export default LoginView;
