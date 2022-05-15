6cdee0938578085d21f0564b52ec292f6dbd679f
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-15 14:16:08 -0500

    chore: create RequiredAuth component

---
+++ react/src/utils/RequiredAuth.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {useAuth} from "@/context/authContext";
+import {Navigate, useLocation} from "react-router-dom";
+
+type RequiredAuthProps = { children: JSX.Element };
+
+const RequiredAuth = ({children}: RequiredAuthProps) => {
+  const auth = useAuth();
+  const location = useLocation();
+
+  if (!auth.isAuth())
+    return <Navigate to="/login" replace state={{from: location}}/>
+
+  return children;
+}
+
+export default RequiredAuth;
