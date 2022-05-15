15158d235d645f4a3e6bcf62447cb0f6266094c0
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-15 11:59:03 -0500

    feat: create useTokens hook
    Simplifies the token fetching

---
+++ react/src/hooks/useTokens.ts
@@ -0,0 +1,27 @@
+import {useState} from "react";
+
+const useTokens = () => {
+  const localStorageAccessToken = localStorage.getItem("accessToken");
+  const localStorageRefreshToken = localStorage.getItem("refreshToken");
+
+  const [accessToken, setAccessToken] = useState(localStorageAccessToken);
+  const [refreshToken, setRefreshToken] = useState(localStorageRefreshToken);
+
+  type SetTokensWrapperProps = { accessToken: string, refreshToken: string };
+  const setTokensWrapper = (props: SetTokensWrapperProps) => {
+    if (props.accessToken) {
+      localStorage.setItem("accessToken", props.accessToken);
+      setAccessToken(props.accessToken);
+    }
+    if (props.refreshToken) {
+      localStorage.setItem("refreshToken", props.refreshToken);
+      setRefreshToken(props.refreshToken);
+    }
+  }
+
+  return {
+    tokens: {accessToken, refreshToken}, setTokens: setTokensWrapper
+  }
+}
+
+export default useTokens;
