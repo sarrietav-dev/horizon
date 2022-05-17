f6b7883f7cb5d0486add3e531bc7f45b77a502f2
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-17 14:25:28 -0500

    fix: make the create button's position fixed.

--- react/src/views/HomeView.tsx
+++ react/src/views/HomeView.tsx
@@ -42,7 +42,7 @@ const CreateButton = ({ onClick }: { onClick: () => void }) => {
   return (
     <div
       style={{ height: "10vh", width: "10vh", bottom: 30, right: 30 }}
-      className="position-absolute rounded-circle bg-primary d-flex justify-content-center align-items-center"
+      className="position-fixed rounded-circle bg-primary d-flex justify-content-center align-items-center"
       onClick={onClick}
     >
       <i className="bi bi-plus text-white fs-1"></i>
