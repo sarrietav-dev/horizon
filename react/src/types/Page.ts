d92c0786930d6a46b90c2d8f777a28740ffe69e7
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-18 20:10:19 -0500

    chore: create interface Page

---
+++ react/src/types/Page.ts
@@ -0,0 +1,16 @@
+export interface Page<T> {
+  content: T;
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  first: boolean;
+  numberOfElements: number;
+  empty: boolean;
+}
