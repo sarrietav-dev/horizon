4c4c81c9d0ea8a7ad72c6309d4c20188bd91ddea
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-18 22:56:51 -0500

    feat: create isAuth attribute to the store and use it on the RequiredAuth.tsx component

--- react/src/stores/reducers/auth.store.ts
+++ react/src/stores/reducers/auth.store.ts
@@ -1,17 +1,26 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import authService from "@/services/authService";
 import { Credentials } from "@/types/Credentials";
+import { RootState } from "@/stores";
+
+const ACCESS_TOKEN = "accessToken";
+const REFRESH_TOKEN = "refreshToken";
+
+const localStorageAccessToken = localStorage.getItem(ACCESS_TOKEN);
+const localStorageRefreshToken = localStorage.getItem(REFRESH_TOKEN);
 
 interface AuthStoreInitialState {
   accessToken: string | null;
   refreshToken: string | null;
   userRole: "ROLE_RESIDENT" | "ROLE_ADMIN" | "ROLE_PROPRIETARY" | null;
+  isAuth: boolean;
 }
 
 const initialState: AuthStoreInitialState = {
-  accessToken: null,
-  refreshToken: null,
+  accessToken: localStorageAccessToken,
+  refreshToken: localStorageRefreshToken,
   userRole: null,
+  isAuth: localStorageAccessToken !== null && localStorageRefreshToken !== null,
 };
 
 export const authSlice = createSlice({
@@ -21,20 +30,25 @@ export const authSlice = createSlice({
     deleteTokens(state) {
       state.accessToken = null;
       state.refreshToken = null;
-      localStorage.removeItem("accessToken");
-      localStorage.removeItem("refreshToken");
+      localStorage.removeItem(ACCESS_TOKEN);
+      localStorage.removeItem(REFRESH_TOKEN);
+      state.isAuth = false;
     },
   },
   extraReducers: (builder) => {
     builder.addCase(authenticateUser.fulfilled, (state, action) => {
       const { accessToken, refreshToken } = action.payload;
       state.accessToken = accessToken;
-      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem(ACCESS_TOKEN, accessToken);
       state.refreshToken = refreshToken;
-      localStorage.setItem("refreshToken", refreshToken);
+      localStorage.setItem(REFRESH_TOKEN, refreshToken);
+      state.isAuth = true;
     });
   },
 });
+
+export const isAuth = (state: RootState) =>
+  state.auth.accessToken !== null && state.auth.refreshToken !== null;
 
 export default authSlice.reducer;
 
