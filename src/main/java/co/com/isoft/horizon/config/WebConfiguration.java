961283e7e2f94fef35df612fb96343d159ca55a0
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-05-27 04:20:33 -0500

    fix: disable cors

--- src/main/java/co/com/isoft/horizon/config/WebConfiguration.java
+++ src/main/java/co/com/isoft/horizon/config/WebConfiguration.java
@@ -1,6 +1,7 @@
 package co.com.isoft.horizon.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,9 @@ public class WebConfiguration implements WebMvcConfigurer {
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.addViewController("/{spring:\\w+}").setViewName("forward:/");
+  }
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedMethods("*");
   }
 }
